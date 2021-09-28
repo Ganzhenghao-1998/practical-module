407a686ed72a91dfd322adcf2473e0762b8e1a17
Ganzhenghao <13558716290@163.com>
2021-09-28 11:17:51 +0800

    添加防重复提交注解项目的测试模块

---
+++ prevent-test/src/main/java/com/example/prevent/PreventTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.prevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreventTestApplication.class, args);
+    }
+
+}
---
+++ prevent-test/src/test/java/com/example/prevent/PreventTestApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.prevent;
+
+import com.ganzhenghao.prsa.annotation.NoRepeatCommit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventTestApplicationTests {
+
+    @Autowired
+    private NoRepeatCommit noRepeatCommit;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testMethodName() {
+        System.out.println(noRepeatCommit.redisKeyPrefix());
+        System.out.println(noRepeatCommit.headerName());
+    }
+
+}
